da852689144de8fc211c5660ed55a502b457b17a
itsdragonic <142061105+itsdragonic@users.noreply.github.com>
2023-09-02 15:07:46 -0500

    Added PID

--- 72116A/src/main.cpp
+++ 72116A/src/main.cpp
@@ -1,35 +1,114 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
-/*    Author:       lucya                                                     */
+/*    Author:       CHS Robotics                                              */
 /*    Created:      9/2/2023, 12:45:16 PM                                     */
 /*    Description:  V5 project                                                */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
+// vscode.dev/
 
 #include "vex.h"
 
 using namespace vex;
 
-// A global instance of competition
 competition Competition;
 
-// define your global instances of motors and other devices here
-
-/*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
-/*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the V5 has been powered on and        */
-/*  not every time that the robot is disabled.                               */
-/*---------------------------------------------------------------------------*/
 
 void pre_auton(void) {
 
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
+}
+
+// Settings (adjust these values)
+double kP = 0.0;
+double kI = 0.0;
+double kD = 0.0;
+double turnkP = 0.0;
+double turnkI = 0.0;
+double turnkD = 0.0;
+
+// Autonomous Settings
+int desiredValue = 200;
+int desiredTurnValue = 0;
+
+int error; // SensorValue - DesiredValue (delta X) : Positional -> speed -> acceleration
+int prevError = 0; // Position 20 miliseconds ago
+int derivative; // error - prevError : Speed (helps with friction, battery voltage)
+int totalError = 0; // totalError = totalError + error
+
+int turnError; // SensorValue - DesiredValue (delta X) : Positional -> speed -> acceleration
+int turnPrevError = 0; // Position 20 miliseconds ago
+int turnDerivative; // error - prevError : Speed (helps with friction, battery voltage)
+int turnTotalError = 0; // totalError = totalError + error
+
+bool resetDriveSensors = false;
+
+// Variables modified for use
+bool enableDrivePID = true;
+
+int drivePID() {
+
+  while(enableDrivePID) {
+
+    if (resetDriveSensors) {
+      resetDriveSensors = false;
+      LeftMotor.setPosition(0,degrees);
+      RightMotor.setPosition(0,degrees);
+      // https://api.vexcode.cloud/v5/html/
+    }
+
+    // Positions of both motors
+    int leftMotorPosition = LeftMotor.position(degrees);
+    int rightMotorPosition = RightMotor.position(degrees);
+
+    /////////////////////////////////////////////////////////////////
+    // Lateral movement PID
+    /////////////////////////////////////////////////////////////////
+
+    // Average of 2 motors
+    int averagePosition = (leftMotorPosition + rightMotorPosition)/2;
+
+    // Potential
+    error = averagePosition - desiredValue;
+
+    // Derivative
+    derivative - error - prevError;
+
+    // Integral : Velocity -> Position -> Absement
+    totalError += error;
+
+    double lateralMotorPower = error * kP + derivative * kD + totalError * kI;
+
+    /////////////////////////////////////////////////////////////////
+    // Turning movement PID
+    /////////////////////////////////////////////////////////////////
+
+    // Average of 2 motors
+    int turnDifference = leftMotorPosition - rightMotorPosition;
+
+    // Potential
+    turnError = turnDifference - desiredTurnValue;
+
+    // Derivative
+    turnDerivative - turnError - turnPrevError;
+
+    // Integral : Velocity -> Position -> Absement
+    turnTotalError += turnError;
+
+    double turnMotorPower = turnError * turnkP + turnDerivative * turnkD + turnTotalError * turnkI;
+
+    /////////////////////////////////////////////////////////////////
+
+    LeftMotor.spin(forward, lateralMotorPower + turnMotorPower, voltageUnits::volt);
+    RightMotor.spin(forward, lateralMotorPower - turnMotorPower, voltageUnits::volt);
+
+    prevError = error;
+    turnPrevError = turnError;
+    vex::task::sleep(20);
+
+  }
+
+  return 1;
 }
 
 /*---------------------------------------------------------------------------*/
@@ -43,9 +122,18 @@ void pre_auton(void) {
 /*---------------------------------------------------------------------------*/
 
 void autonomous(void) {
-  // ..........................................................................
-  // Insert autonomous user code here.
-  // ..........................................................................
+  
+  vex::task weWinThese(drivePID);
+
+  resetDriveSensors = true;
+  desiredValue = 300;
+  desiredTurnValue = 600;
+
+  vex::task::sleep(100);
+
+  resetDriveSensors = true;
+  desiredValue = 300;
+  desiredTurnValue = 300;
 }
 
 /*---------------------------------------------------------------------------*/
@@ -58,20 +146,20 @@ void autonomous(void) {
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
-void usercontrol(void) {
-  // User control code here, inside the loop
-  while (1) {
-    // This is the main execution loop for the user control program.
-    // Each time through the loop your program should update motor + servo
-    // values based on feedback from the joysticks.
+void usercontrol(void) { 
+  enableDrivePID = false;
 
-    // ........................................................................
-    // Insert user code here. This is where you use the joystick values to
-    // update your motors, etc.
-    // ........................................................................
+  while (true) {
+    double turnVal = Controller1.Axis1.position(percent);
+    double forwardVal = Controller1.Axis3.position(percent);
 
-    wait(20, msec); // Sleep the task for a short amount of time to
-                    // prevent wasted resources.
+    double turnVolts = turnVal * 0.12;
+    double forwardVolts = forwardVal * 0.12 * (1 - (std::abs(turnVolts)/12.0) * turnImportance);
+
+    LeftMotor.spin(forward, forwardVolts + turnVolts, voltageUnits::volt);
+    RightMotor.spin(forward, forwardVolts - turnVolts, voltageUnits::volt);
+
+    wait(20, msec);
   }
 }
 
